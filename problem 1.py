0b3d56f7897dc8b0b7601f611079d7c840a796af
Mqsed-123 <146757727+Mqsed-123@users.noreply.github.com>
2024-03-05 22:55:52 +0200

    Add files via upload

---
+++ problem 1.py
@@ -0,0 +1,21 @@
+#problem 1
+print ("please enter the grade")
+grade = float (input())
+if grade >=90 :
+    print('Your Grade Is "A+"')
+elif grade >=85:
+    print ('Your Grade Is"A"')
+elif grade >=80:
+    print ('Your Grade Is"B+"')
+elif grade >=75:
+    print ('Your Grade Is"B"')
+elif grade >=70:
+    print ('Your Grade Is"C+"')
+elif grade >=65:
+    print ('Your Grade Is"C"')
+elif grade >=60:
+    print ('Your Grade Is"D+"')
+elif grade >=50:
+    print ('Your Grade Is"D"')
+else:
+    print ('Your Grade Is"F"')
